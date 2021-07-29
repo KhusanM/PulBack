12cd752c83bb64d4fc6d833f9d15662954cb4297
KhusanM <khusanmasharipov1998@gmail.com>
2021-07-29 16:12:59 +0500

    Initial Commit

---
+++ PulBack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PulBack
+//
+//  Created by Kh's MacBook on 29.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
